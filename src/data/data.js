8d88e9f8bad6f9c8305777ee8bb616231bb54a29
EscobarLuis <escobarluis77@gmail.com>
2025-07-21 23:13:15 -0300

    Update data.js
    elimino datos de configuracion de firebase

--- src/data/data.js
+++ src/data/data.js
@@ -5,12 +5,12 @@ import { getFirestore } from "firebase/firestore";
 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyC7idVmwx27VM8MX5YnJQyTJOnPBqZzcmE",
-    authDomain: "api-node-8778d.firebaseapp.com",
-    projectId: "api-node-8778d",
-    storageBucket: "api-node-8778d.firebasestorage.app",
-    messagingSenderId: "1:998577915511:web:efaf980c67a09a332ee872",
-    appId: "998577915511"
+    apiKey: ,
+    authDomain: ,
+    projectId: ,
+    storageBucket: ,
+    messagingSenderId: ,
+    appId: 
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
